a9f7fa57d1d4ea013e7a7e74375722fb2bb3161d
danikkk <dldanown@gmail.com>
2020-04-19 21:07:21 +0300

    W.I.P

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// interface User {
+//   id: string;
+//   username: string;
+//   password: string;
+//   email: string;
+//   name: string;
+//   surname: string;
+//   accounts: Account[];
+// }
+
+// interface Account {
+//   id: string;
+//   name: string;
+//   type: string;
+//   currency: string;
+// }
+
+class App extends Component {
+  state = {
+    users: [],
+    isLoading: false,
+  };
+
+  async componentDidMount() {
+    //this.setState({isLoading: true})
+    const response = await fetch("/api/users", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((users) => this.setState({ users, isLoading: false }));
+  }
+  render() {
+    const { users, isLoading } = this.state;
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    const resume = users.map((user: any) => (
+      <div key={user.id}>{user.name}</div>
+    ));
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {resume}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
